494e424a3c713f4adfeb564c273e7bdf564fdf15
Robert Kocsis <robertzkocsis@gmail.com>
2024-02-20 21:44:09 +0200

    Add tailwind css and add shadcn-ui with card component

--- src/lib/index.ts
+++
@@ -1,5 +0,0 @@
-import type { Activity } from '$lib/entities/Activity';
-import type { Cost } from '$lib/entities/Cost';
-import type { Day } from '$lib/entities/Day';
-import type { Trip } from '$lib/entities/Trip';
-export type { Activity, Cost, Day, Trip };
